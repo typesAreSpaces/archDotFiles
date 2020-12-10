fba0e27f46497e3dbeac0764b4afcb02cbeadcab
jose <jabelcastellanosjoo@unm.edu>
2020-12-09 21:50:19 -0700

    Update arch-machine-setup bash.

--- .arch-machine-setup.sh
+++ .arch-machine-setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+sudo pacman -S zsh
+/bin/zsh
+
 # ------------------------------------------------------------------------
 # Dot files setup
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 echo ".cfg" >> .gitignore
 git clone --bare git@github.com:typesAreSpaces/archDotFiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 mkdir -p .config-backup && \
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
   xargs -I{} mv {} .config-backup/{}
-config checkout
-config config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 source .zshrc
 # ------------------------------------------------------------------------
 # Vim setup
@@ -27,3 +28,4 @@ vim +PlugInstall +qa
 # Installing my usual stuff
 installArchPackages()
 # ------------------------------------------------------------------------
+chsh -s /bin/zsh
