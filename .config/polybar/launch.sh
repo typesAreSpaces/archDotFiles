446d2e4d40dc796fadf53ef1bae13f2cc010be26
jose <jabelcastellanosjoo@unm.edu>
2021-01-09 12:07:34 -0700

    Update polybar files.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar -c ~/.config/polybar/i3_config.ini main &
+  interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
+  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
 done
 
