8124461f825cda8393669c2b87c22ae75f185817
jose <jabelcastellanosjoo@unm.edu>
2021-03-19 23:21:01 -0600

    Add dual screen script generated with arandr. Setup bspwm to support dual screen.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,8 +8,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
-  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
-done
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+  #interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
+  #NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  #done
+
+interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
+NETWORK_INTERFACE=$interface MONITOR=DP3 polybar -c ~/.config/polybar/config.ini main &
+NETWORK_INTERFACE=$interface MONITOR=eDP1 polybar -c ~/.config/polybar/config.ini main &
 
