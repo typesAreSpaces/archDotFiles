0521a92faa8142f78a1075244cee84c051a68c83
jose <jabelcastellanosjoo@unm.edu>
2021-11-14 02:15:27 -0700

    Fix updateMachine script.

--- .local/scripts/updateMachine.sh
+++ .local/scripts/updateMachine.sh
@@ -1,6 +1,5 @@
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout master
 config pull
-branch_name=config rev-parse --abbrev-ref HEAD
-config checkout $branch_name
+config checkout $1
 config merge master
