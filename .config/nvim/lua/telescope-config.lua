0a41299f97b773616b1f8f0d77ab887713a5a28a
jose <jabelcastellanosjoo@unm.edu>
2021-12-04 14:02:20 -0700

    Update telescope settings.

--- .config/nvim/lua/telescope-config.lua
+++ .config/nvim/lua/telescope-config.lua
@@ -19,6 +19,15 @@ require('telescope').setup{
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+    find_files = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+    },
+    buffers = {
+      theme = "dropdown",
+    }
   },
   extensions = {
     -- Your extension configuration goes here:
