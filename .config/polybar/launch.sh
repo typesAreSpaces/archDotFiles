0145957b229dd324bc3e3382d69354cd37ac61de
jose <jabelcastellanosjoo@unm.edu>
2021-12-10 11:56:12 -0700

    Plan to include a second bar for polybar.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,6 +12,7 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
   #interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
   #interface="wlan0";
   interface="enp2s0";
-  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini top_bar &
+  #NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom_bar &
 done
 
