6becc37a8989c9a1a07260e7e22e0ed9a2501e5f
jose <jabelcastellanosjoo@unm.edu>
2021-12-10 20:36:58 -0700

    Refactor nvim config and add lazy loading setup.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,51 +1,45 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local packer_url = 'https://github.com/wbthomason/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
-end
-
-vim.cmd [[packadd packer.nvim]]
-vim.cmd([[
-augroup packer_user_config
-autocmd!
-autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-augroup end
-]])
-
 return require('packer').startup(function()
   -- # Packer can manage itself
   use { 'wbthomason/packer.nvim', 
   cmd = { 'PackerCompile', 'PackerInstall', 'PackerUpdate', 'PackerClean', 'PackerSync', 'PackerLoad' } }
 
   -- # IDE Experience
-  use 'williamboman/nvim-lsp-installer'
-  use 'neovim/nvim-lspconfig'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'nvim-treesitter/playground'
+  use { 'williamboman/nvim-lsp-installer', event = "BufWinEnter", config = get_config("lsp-install") }
+  -- Install texlab using paru
+  -- not nvim-lsp-installer 
+  use { 'neovim/nvim-lspconfig', config = get_config("lsp") }
+  use { 'nvim-treesitter/nvim-treesitter', 
+  run = ':TSUpdate', 
+  event = "BufWinEnter",
+  config = get_config("treesitter") }
+  use { 'nvim-treesitter/playground', after = "nvim-treesitter" }
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+  use 'quangnguyen30192/cmp-nvim-ultisnips'
+  use { 'hrsh7th/nvim-cmp', config = get_config("cmp") }
   use { 'nvim-telescope/telescope.nvim', 
-  requires = { {'nvim-lua/plenary.nvim'} }}
+  requires = { {'nvim-lua/plenary.nvim'} },
+  cmd = "Telescope",
+  config = get_config("telescope")}
   use 'mbbill/undotree'
   use 'preservim/nerdtree'
   use 'jeffkreeftmeijer/vim-numbertoggle'
   use 'preservim/nerdcommenter'
   use 'puremourning/vimspector'
-  use({ "SirVer/ultisnips", 
+  use { "SirVer/ultisnips", 
   requires = "honza/vim-snippets", 
-  config = function() vim.g.UltiSnipsRemoveSelectModeMappings = 0 end,})
-  use 'quangnguyen30192/cmp-nvim-ultisnips'
-  use 'lervag/vimtex'
+  config = get_config("ultisnips") }
+  use { 'lervag/vimtex', event = "BufWinEnter", config = get_config("vimtex") }
   use 'mhinz/neovim-remote'
   use 'tpope/vim-fugitive'
   use 'skywind3000/asyncrun.vim'
-  use 'folke/which-key.nvim'
-  use { 'gelguy/wilder.nvim', run = ':UpdateRemotePlugins'}
+  use { 'folke/which-key.nvim', event = "BufWinEnter", config = get_config("which-key") }
+  use { 'gelguy/wilder.nvim', 
+  run = ':UpdateRemotePlugins', 
+  event = "BufWinEnter",
+  config = get_config("wilder") }
   use 'MattesGroeger/vim-bookmarks'
 
   -- # Neovim apps 
@@ -64,10 +58,13 @@ return require('packer').startup(function()
 
   -- # Ricing
   use 'mhinz/vim-startify'
-  use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
+  use { 'nvim-lualine/lualine.nvim', 
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }, 
+  event = "BufWinEnter",
+  config = get_config("lualine") }
   use 'ryanoasis/vim-devicons'
 
   -- # Syntax
-  use 'bohlender/vim-smt2' 
+  use {'bohlender/vim-smt2', config = get_config("smt2") }
   use 'plasticboy/vim-markdown'
 end)
