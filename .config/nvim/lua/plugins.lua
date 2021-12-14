5ef188890bba0df359276fe95204f8f1cba755ff
jose <jabelcastellanosjoo@unm.edu>
2021-12-13 17:35:41 -0700

    Minor changes.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,7 +4,9 @@ return require('packer').startup(function()
   cmd = { 'PackerCompile', 'PackerInstall', 'PackerUpdate', 'PackerClean', 'PackerSync', 'PackerLoad' } }
 
   -- # IDE Experience
-  use { 'williamboman/nvim-lsp-installer', event = "BufWinEnter", config = get_config("lsp-install") }
+  use { 'williamboman/nvim-lsp-installer', 
+  event = "BufWinEnter", 
+  config = get_config("lsp-install") }
   -- Install texlab using paru
   -- not nvim-lsp-installer 
   use { 'neovim/nvim-lspconfig', config = get_config("lsp") }
@@ -33,12 +35,13 @@ return require('packer').startup(function()
   use { "SirVer/ultisnips", 
   requires = "honza/vim-snippets", 
   config = get_config("ultisnips") }
-  --use { 'lervag/vimtex', event = "BufWinEnter", config = get_config("vimtex") }
   use { 'lervag/vimtex', config = get_config("vimtex") }
   use 'mhinz/neovim-remote'
   use 'tpope/vim-fugitive'
   use 'skywind3000/asyncrun.vim'
-  use { 'folke/which-key.nvim', event = "BufWinEnter", config = get_config("which-key") }
+  use { 'folke/which-key.nvim', 
+  event = "BufWinEnter", 
+  config = get_config("which-key") }
   use { 'gelguy/wilder.nvim', 
   run = ':UpdateRemotePlugins', 
   event = "BufWinEnter",
