282430974c4df2438bcd58db9026a2c4f8fa4334
jose <jabelcastellanosjoo@unm.edu>
2021-12-20 14:33:32 -0700

    Remove tmuxThemePicker.sh from tmux conf directory. Update emacs config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -108,6 +108,8 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(global-set-key (kbd "C-i") 'evil-jump-forward)
+
 (use-package general
   :after evil
   :config
@@ -626,7 +628,7 @@
   ;; This is set to 't' to avoid mail syncing issues when using mbsync
   (setq mu4e-change-filenames-when-moving t)
 
-  (setq mu4e-update-interval 60)
+  (setq mu4e-update-interval 600)
   (setq mu4e-get-mail-command "mbsync -a")
   (setq mu4e-maildir "~/Mail")
 
