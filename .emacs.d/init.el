26588fcd15b97eba363462546362e12268a875b3
jose <jabelcastellanosjoo@unm.edu>
2021-12-23 17:53:36 -0700

    Update MacMachine.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -769,3 +769,16 @@
 (use-package simpleclip
   :config
   (simpleclip-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(simpleclip yasnippet-snippets which-key vterm visual-fill-column use-package typescript-mode rainbow-delimiters pyvenv python-mode org-tree-slide org-bullets no-littering mu4e-alert lsp-ui lsp-ivy ivy-rich ivy-prescient hide-mode-line helpful general forge flx evil-nerd-commenter evil-collection eterm-256color eshell-git-prompt doom-themes doom-modeline dired-single dired-open dired-hide-dotfiles dashboard dap-mode counsel-projectile company-box command-log-mode auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
