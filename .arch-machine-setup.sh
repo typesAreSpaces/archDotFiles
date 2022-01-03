91b8667a62d1e6602b80e150c28c2c0ac157c967
jose <jabelcastellanosjoo@unm.edu>
2022-01-03 14:27:09 -0700

    Update installation script.

--- .arch-machine-setup.sh
+++ .arch-machine-setup.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
-# ------------------------------------------------------------------------
+CONFIG='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
 # Extra programs
-sudo pacman -S zsh git vim curl
-# ------------------------------------------------------------------------
+sudo pacman -S zsh git
+
 # Dot files setup
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/typesAreSpaces/archDotFiles.git $HOME/.cfg
 mkdir -p .config-backup && \
-  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+  $CONFIG checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
   xargs -I{} mv {} .config-backup/{}
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
-# ------------------------------------------------------------------------
-# Vim setup
-## Download vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-## Create undodir directory
-mkdir -p ~/.vim/undodir
-## Install vim plugins
-vim +PlugInstall +qa
+$CONFIG checkout
+$CONFIG config --local status.showUntrackedFiles no
+
 # ------------------------------------------------------------------------
 # Installing my usual stuff
 sudo pacman -S --needed - < .arch_packages 
+
+# TODO:
+# Install GithubProjects apps including:
+# - bsp-layout
+# - blog
+# - paru
+# - phd-thesis
+# - QuickTex
+# - slimlock (?)
+# - typesAreSpaces.github.io
+# - zathura-pywal
+
 # ------------------------------------------------------------------------
+# TODO: 
+# - Remove polybar since it's in .arch_packages
+# - Keep it for now to replicate installation 
+# - for the above GithubProjects apps
 mkdir -p ~/Documents/GithubProjects/polybar
 git clone https://aur.archlinux.org/polybar.git ~/Documents/GithubProjects/polybar
 cd ~/Documents/GithubProjects/polybar && makepkg -si
+
+# Change shell to zsh
 chsh -s /bin/zsh
