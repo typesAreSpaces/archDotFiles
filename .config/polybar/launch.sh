171ab28f19055edb8de58c297af12f3024b583ba
jose <jabelcastellanosjoo@unm.edu>
2022-03-14 19:17:46 -0600

    Update USB-Stick.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,8 +8,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  interface="$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')";
   NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini top_bar &
   NETWORK_INTERFACE=$interface MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom_bar &
 done
