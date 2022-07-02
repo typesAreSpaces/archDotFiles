13c8debc4cf2eb256270cf13eea09fbba579ba04
jose <jabelcastellanosjoo@unm.edu>
2022-07-02 12:59:26 -0600

    Reorganize bspwm config files.

--- .local/scripts/autostart.sh
+++ .local/scripts/autostart.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
+
 setxkbmap
 xmodmap $HOME/.Xmodmap
-~/.local/scripts/UnseenMail/keep_checking_email.sh &
-emacs --daemon
+[ -f $HOME/.xsessionrc ] && source $HOME/.xsessionrc
+[ "$(ps aux | grep 'bin.*keep_checking_email.sh' | wc -l | xargs)" = "1" ] && $HOME/.local/scripts/UnseenMail/keep_checking_email.sh&
+[ "$(ps aux | grep 'emacs' | wc -l | xargs)" = "1" ] && emacs --daemon
