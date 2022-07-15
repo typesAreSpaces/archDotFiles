ec81227ad4682ecd30ffe1952f15b6faa511ec44
jose <jabelcastellanosjoo@unm.edu>
2022-07-14 21:18:59 -0600

    Add toggleKBD script.

--- .local/scripts/autostart.sh
+++ .local/scripts/autostart.sh
@@ -2,6 +2,7 @@
 
 setxkbmap
 xmodmap $HOME/.Xmodmap
+echo "CTRL" > ~/.current_binding
 [ -f $HOME/.xsessionrc ] && source $HOME/.xsessionrc
 [ "$(ps aux | grep 'bin.*keep_checking_email.sh' | wc -l | xargs)" = "1" ] && $HOME/.local/scripts/UnseenMail/keep_checking_email.sh&
 [ "$(ps aux | grep 'emacs' | wc -l | xargs)" = "1" ] && emacs --daemon
