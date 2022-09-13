7531fc6fedad8409f36ede4eec53f23af3ac27a4
jose <jabelcastellanosjoo@unm.edu>
2022-09-13 11:48:39 -0600

    Add mbsync toggle in keep_checking_email script.

--- .local/scripts/mail_scripts/keep_checking_email.sh
+++ .local/scripts/mail_scripts/keep_checking_email.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# To enable mbsync write anything on the file $ENABLE_MBSYNC
+# To disable mbsync make $ENABLE_MBSYNC an empty file
+
+ENABLE_MBSYNC=/home/jose/.enable_mbsync
 FILE=/home/jose/.unread_email
 DIR=/home/jose/Mail/unm/Inbox/new
 
 while [ 1 ]; do
   # python ~/.local/scripts/UnseenMail/UnseenMail.py
-  mbsync -a
+  if [ -s $ENABLE_MBSYNC ]; then mbsync -a; fi
   echo ' '> $FILE
   ls $DIR | wc -l | xargs >> $FILE
   sleep 15;
